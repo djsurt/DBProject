2fb70973fc049f76ec53fc5c486cab231849c85f
georgia-martinez <gcm49@case.edu>
2022-11-22 15:39:03 -0500

    Added TableDropdown class with ComboBox info

--- MainCode/src/Main.java
+++ MainCode/src/Main.java
@@ -1,11 +1,11 @@
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.scene.control.Button;
 
 public class Main extends Application {
 
@@ -15,28 +15,38 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Software Engineering Jobs Database");
+        primaryStage.setTitle("Computer Science Jobs Database");
 
         // Setting up tabs
         TabPane tabPane = new TabPane();
 
         tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE); // removing close button from tabs
 
-        Tab tab1 = new Tab("Planes");
-        Tab tab2 = new Tab("Cars");
-        Tab tab3 = new Tab("Boat");
+        Tab insertTab = new Tab("Insert");
+        Tab deleteTab = new Tab("Delete");
 
-        tabPane.getTabs().addAll(tab1, tab2, tab3);
+        tabPane.getTabs().addAll(insertTab, deleteTab);
 
         VBox mainBox = new VBox(tabPane);
 
-        Button button = new Button("Click Me");
-
+        // Setting up the insert tab
         VBox vbox1 = new VBox();
-        tab1.setContent(vbox1);
 
-        vbox1.getChildren().add(button);
+        insertTab.setContent(vbox1);
         vbox1.setPadding(new Insets(10));
+
+        ComboBox<String> comboBox = TableDropdown.getComboBox();
+
+        vbox1.getChildren().addAll(comboBox);
+
+        // Setting up the delete tab
+        VBox vbox2 = new VBox();
+        deleteTab.setContent(vbox2);
+        vbox2.setPadding(new Insets(10));
+
+        ComboBox<String> comboBox2 = TableDropdown.getComboBox();
+
+        vbox2.getChildren().addAll(comboBox2);
 
         // Setting up the stage
         Scene scene = new Scene(mainBox);
