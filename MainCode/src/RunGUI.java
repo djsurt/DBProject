025197130eb500ba27772af687ceea6b7f9c21b4
georgia-martinez <gcm49@case.edu>
2022-11-29 22:29:04 -0500

    Created main window and tabs

--- MainCode/src/RunGUI.java
+++ MainCode/src/RunGUI.java
@@ -1,6 +1,24 @@
+import javax.swing.*;
+
 public class RunGUI {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        JFrame frame = new JFrame();
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setExtendedState(frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+        frame.setVisible(true);
+
+        JTabbedPane tabbedPane = new JTabbedPane();
+
+        JPanel getStarted = new JPanel();
+        JPanel insertPanel = new JPanel();
+
+        tabbedPane.addTab("Get Started", getStarted);
+        tabbedPane.addTab("Insert", insertPanel);
+
+//        tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+
+        frame.getContentPane().add(tabbedPane);
     }
 }
